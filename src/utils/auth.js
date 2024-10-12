4ecbd6b9c46793a64454815b00906207eea0694e
gillty <gooee@mac.com>
2024-10-12 10:14:06 -0500

    api url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,7 @@
 import { processServerRequest } from "./utils";
 
 const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:3002";
+console.log("Base URL:", baseUrl);
 
 if (import.meta.env.NODE_ENV === "production") {
   console.log("Running in production mode");
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV || "development"
+    ),
+  },
   server: {
     port: 3000,
+    // Uncomment and configure the proxy if needed
     // proxy: {
     //   "/api": {
     //     target: "http://localhost:3001",
